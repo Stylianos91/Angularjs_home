822a588743c4cd9a9667e2a73d63883dae516599
Stelios Manousakis <stmanousakis@yahoo.gr>
2018-02-13 23:23:28 +0100

    fixed angular

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainControlller', ['$scope', '$filter', function($scope, $filter) {
     
+    $scope.handle = '';
     
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
     
 }]);
