8ce7b283fefee6acbdc85aea57374e8e7fe5fcaa
Stelios Manousakis <stmanousakis@yahoo.gr>
2018-02-15 23:29:37 +0100

    watchers example

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainCtrl1', ['$scope', '$filter', function($scope, $filter) {
+myApp.controller('mainCtrl1', ['$scope', '$filter', '$timeout', function($scope, $filter,$timeout) {
     
     $scope.handle = '';
     
     $scope.lowercasehandle = function() {
+    	console.log("afawfaawfa");
         return $filter('lowercase')($scope.handle);
     };
     
+    
+    $scope.$watch('handle', function(newValue, oldValue){
+    	console.info('Changed');
+    	console.log('Old ' + oldValue);
+    	console.log('New ' + newValue);
+    });
+    
+    
+    
+    $timeout(function() {
+    	// to   $timeout  kanei to idio me ta commented
+   // 	$scope.$apply(function(){
+    		
+		$scope.handle='newtwitterhandle';
+		console.log('scope changed!!!');
+		
+  //  	})
+    	
+    	}, 3000);
+    
+    
+    
 }]);
+
+
+
+
+
 
 myApp.controller('mainCtrl2', ['$scope', '$filter', function($scope, $filter) {
     
